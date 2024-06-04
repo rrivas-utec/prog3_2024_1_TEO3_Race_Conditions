7c48b2ddd69117fe2b482286119ed0e4d3facee1
rrivas <rrivas@utec.edu.pe>
2024-06-03 21:21:50 -0500

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -104,11 +104,146 @@ void generar_totales() {
         std::cout << vtotales[i] << " ";
 }
 
+class Cliente {
+    std::mutex mtx;
+    double saldo = 0;
+public:
+    Cliente() = default;
+    Cliente(double saldo_inicial): saldo(saldo_inicial) {}
+    void deposito(double importe) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(4));
+        std::lock_guard lg(mtx);
+        saldo += importe;
+    }
+    void retiro(double importe) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(4));
+        std::unique_lock lg(mtx);
+        saldo -= importe;
+        lg.unlock();
+    }
+    friend std::ostream& operator << (std::ostream& os, const Cliente& c) {
+        os << c.saldo;
+        return os;
+    }
+
+    void simulacion_metodo(int nd, int nr) {
+        std::vector<std::jthread> vt;
+        vt.reserve(nd+nr);
+
+        for (int i = 0; i < nd; ++i) {
+            vt.emplace_back(&Cliente::deposito, this, 1);
+        }
+        for (int i = 0; i < nr; ++i) {
+            vt.emplace_back([this]() { retiro(1); });
+        }
+        for (int i = 0; i < nd + nr; ++i) {
+            vt[i].join();
+        }
+        std::cout << *this << " ";
+    }
+};
+
+class ClienteAtomico {
+//    double saldo = 0;
+    std::atomic<double> saldo = 0;
+public:
+    ClienteAtomico() = default;
+    ClienteAtomico(double saldo_inicial): saldo(saldo_inicial) {}
+    void deposito(double importe) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(4));
+        saldo += importe;
+    }
+    void retiro(double importe) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(4));
+        saldo -= importe;
+    }
+    friend std::ostream& operator << (std::ostream& os, const ClienteAtomico& c) {
+        os << c.saldo;
+        return os;
+    }
+
+    void simulacion_metodo(int nd, int nr) {
+        std::vector<std::jthread> vt;
+        vt.reserve(nd+nr);
+
+        for (int i = 0; i < nd; ++i) {
+            vt.emplace_back(&ClienteAtomico::deposito, this, 1);
+        }
+        for (int i = 0; i < nr; ++i) {
+            vt.emplace_back([this]() { retiro(1); });
+        }
+        for (int i = 0; i < nd + nr; ++i) {
+            vt[i].join();
+        }
+        std::cout << *this << " ";
+    }
+};
+
+void simulacion_funcion(int nd, int nr) {
+    Cliente c1;
+    std::vector<std::jthread> vt;
+    vt.reserve(nd+nr);
+
+    for (int i = 0; i < nd; ++i) {
+        vt.emplace_back(&Cliente::deposito, &c1, 1);
+    }
+    for (int i = 0; i < nr; ++i) {
+        vt.emplace_back(&Cliente::retiro, &c1, 1);
+    }
+    for (int i = 0; i < nd + nr; ++i) {
+        vt[i].join();
+    }
+    std::cout << c1 << " ";
+}
+
+void simulacion_funcion_2(int nd, int nr) {
+    ClienteAtomico c1;
+    std::vector<std::jthread> vt;
+    vt.reserve(nd+nr);
+
+    for (int i = 0; i < nd; ++i) {
+        vt.emplace_back(&ClienteAtomico::deposito, &c1, 1);
+    }
+    for (int i = 0; i < nr; ++i) {
+        vt.emplace_back(&ClienteAtomico::retiro, &c1, 1);
+    }
+    for (int i = 0; i < nd + nr; ++i) {
+        vt[i].join();
+    }
+    std::cout << c1 << " ";
+}
+
+void ejemplo_simulaciones() {
+    int m = 100;
+    for (int i = 0; i < m; ++i) {
+        simulacion_funcion(100, 50);
+    }
+}
+
+void ejemplo_simulaciones_2() {
+    int m = 100;
+    for (int i = 0; i < m; ++i) {
+        simulacion_funcion_2(100, 50);
+    }
+}
+
+void ejemplo_simulaciones_3() {
+    Cliente c1;
+    int m = 100;
+    for (int i = 0; i < m; ++i) {
+        c1.simulacion_metodo(100, 50);
+    }
+}
+
 int main() {
 //    ejemplo_1();
 //    ejemplo_2();
 //    ejemplo_3();
 //    std::cout << incrementar_total(20, 2) << std::endl;
-    generar_totales();
+//    generar_totales();
+//    ejemplo_simulaciones();
+    ejemplo_simulaciones_2();
+//    ejemplo_simulaciones_3();
+
     return 0;
 }
